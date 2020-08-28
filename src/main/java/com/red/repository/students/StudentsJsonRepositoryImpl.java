0edf60761d65e56ef6d41c67a1bfccffb0e26a82
Krzysztof Dragan <Krzysztof.Dragan@stepstone.com>
2020-08-28 11:52:35 +0200

    indexing

--- src/main/java/com/red/repository/students/StudentsJsonRepositoryImpl.java
+++ src/main/java/com/red/repository/students/StudentsJsonRepositoryImpl.java
@@ -4,11 +4,18 @@ import com.red.configuration.DataSourceConfiguration;
 import com.red.model.Student;
 import io.jsondb.JsonDBTemplate;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class StudentsJsonRepositoryImpl implements StudentsRepository {
 
     private final JsonDBTemplate jsonDBTemplate = DataSourceConfiguration.jsonDBTemplate();
-    static Long idIndex = 0L;
+    private AtomicInteger ID_GENERATOR = new AtomicInteger(getLastIndex());
+
+    private int getLastIndex() {
+        var listOfAllStudents = findAll();
+        if (listOfAllStudents.isEmpty()) return 1000;
+        return Math.toIntExact(listOfAllStudents.get(listOfAllStudents.size() - 1).getId()) + 1;
+    }
 
     @Override
     public List<Student> findAll() {
@@ -17,9 +24,8 @@ public class StudentsJsonRepositoryImpl implements StudentsRepository {
 
     @Override
     public void createStudent(Student student) {
-        student.setId(idIndex);
+        student.setId((long) ID_GENERATOR.getAndIncrement());
         jsonDBTemplate.insert(student);
-        idIndex++;
     }
 
     @Override
